f1bc3b3f41c612f83c119351494b2048ec4d729b
Artem Porubai <a.porubai@doc.ua>
2021-11-26 15:24:56 +0200

    feat: get categories by views

--- wrappers/watch/watch.go
+++ wrappers/watch/watch.go
@@ -24,7 +24,7 @@ type WatchWrapper struct {
 
 func NewWatchWrapper(apiUrl string) IWatchWrapper {
 	return &WatchWrapper{baseWrapper: wrappers.GetBaseWrapper(), defaultTimeout: 5 * time.Second,
-		apiUrl: common.StripSlashFromUrl(apiUrl),
+		apiUrl:      common.StripSlashFromUrl(apiUrl),
 		serviceName: "watch-backend"}
 }
 
@@ -44,6 +44,22 @@ type GetLatestWatchesByUserRequest struct {
 	LimitPerUser int     `json:"limit_per_user"`
 	UserIds      []int64 `json:"user_ids"`
 	MinPercent   float64 `json:"min_percent"`
+}
+
+type CategoryInfo struct {
+	Id         int64  `json:"id"`
+	Name       string `json:"name"`
+	ViewsCount int64  `json:"views_count"`
+}
+
+type GetCategoriesByViewsRequest struct {
+	Limit  int64
+	Offset int64
+}
+
+type GetCategoriesResponseChan struct {
+	Error *rpc.RpcError  `json:"error"`
+	Items []CategoryInfo `json:"items"`
 }
 
 func (w *WatchWrapper) GetLastWatchesByUsers(userIds []int64, limitPerUser int, apmTransaction *apm.Transaction,
@@ -68,6 +84,45 @@ func (w *WatchWrapper) GetLastWatchesByUsers(userIds []int64, limitPerUser int, 
 
 		if len(resp.Result) > 0 {
 			items := map[int64][]LastWatchesByUserRecord{}
+
+			if err := json.Unmarshal(resp.Result, &items); err != nil {
+				result.Error = &rpc.RpcError{
+					Code:    error_codes.GenericMappingError,
+					Message: err.Error(),
+					Data:    nil,
+				}
+			} else {
+				result.Items = items
+			}
+		}
+
+		respCh <- result
+	})
+
+	return respCh
+}
+
+func (w *WatchWrapper) GetCategoriesByViews(limit int64, offset int64, apmTransaction *apm.Transaction) chan GetCategoriesResponseChan {
+	respCh := make(chan GetCategoriesResponseChan, 2)
+
+	respChan := w.baseWrapper.SendRpcRequest(w.apiUrl, "GetCategoriesByViews", GetCategoriesByViewsRequest{
+		Limit:  limit,
+		Offset: offset,
+	}, w.defaultTimeout, apmTransaction, w.serviceName, false)
+
+	w.baseWrapper.GetPool().Submit(func() {
+		defer func() {
+			close(respCh)
+		}()
+
+		resp := <-respChan
+
+		result := GetCategoriesResponseChan{
+			Error: resp.Error,
+		}
+
+		if len(resp.Result) > 0 {
+			items := make([]CategoryInfo, 0)
 
 			if err := json.Unmarshal(resp.Result, &items); err != nil {
 				result.Error = &rpc.RpcError{
