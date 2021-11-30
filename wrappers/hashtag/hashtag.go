5669b3303ef9c8b981933304a7c3bc809e5f76cc
Artem Porubai <a.porubai@doc.ua>
2021-11-30 20:32:42 +0200

    feat: omit hashtags

--- wrappers/hashtag/hashtag.go
+++ wrappers/hashtag/hashtag.go
@@ -18,7 +18,7 @@ type Wrapper struct {
 }
 
 type IHashtagWrapper interface {
-	GetHashtagsInternal(hashtags []string, limit int, offset int, apmTransaction *apm.Transaction, forceLog bool) chan HashtagsGetInternalResponseChan
+	GetHashtagsInternal(hashtags []string, omitHashtags []string, limit int, offset int, apmTransaction *apm.Transaction, forceLog bool) chan HashtagsGetInternalResponseChan
 }
 
 func NewHashtagWrapper(apiUrl string) IHashtagWrapper {
@@ -26,16 +26,18 @@ func NewHashtagWrapper(apiUrl string) IHashtagWrapper {
 		baseWrapper:    wrappers.GetBaseWrapper(),
 		defaultTimeout: 5 * time.Second,
 		apiUrl:         common.StripSlashFromUrl(apiUrl),
-		serviceName:    "content-backend"}
+		serviceName:    "content-backend",
+	}
 }
 
-func (w *Wrapper) GetHashtagsInternal(hashtags []string, limit int, offset int, apmTransaction *apm.Transaction, forceLog bool) chan HashtagsGetInternalResponseChan {
+func (w *Wrapper) GetHashtagsInternal(hashtags []string, omitHashtags []string, limit int, offset int, apmTransaction *apm.Transaction, forceLog bool) chan HashtagsGetInternalResponseChan {
 	respCh := make(chan HashtagsGetInternalResponseChan, 2)
 
 	respChan := w.baseWrapper.SendRpcRequest(w.apiUrl, "GetHashtagsInternal", GetHashtagsInternalRequest{
-		Hashtags: hashtags,
-		Limit:    limit,
-		Offset:   offset,
+		Hashtags:     hashtags,
+		OmitHashtags: omitHashtags,
+		Limit:        limit,
+		Offset:       offset,
 	}, w.defaultTimeout, apmTransaction, w.serviceName, forceLog)
 
 	w.baseWrapper.GetPool().Submit(func() {
@@ -54,9 +56,9 @@ func (w *Wrapper) GetHashtagsInternal(hashtags []string, limit int, offset int, 
 
 			if err := json.Unmarshal(resp.Result, &data); err != nil {
 				result.Error = &rpc.RpcError{
-					Code:    error_codes.GenericMappingError,
-					Message: err.Error(),
-					Data:    nil,
+					Code:     error_codes.GenericMappingError,
+					Message:  err.Error(),
+					Data:     nil,
 					Hostname: w.baseWrapper.GetHostName(),
 				}
 			} else {
--- wrappers/hashtag/mock.go
+++ wrappers/hashtag/mock.go
@@ -3,11 +3,11 @@ package hashtag
 import "go.elastic.co/apm"
 
 type HashtagWrapperMock struct {
-	GetHashtagsInternalFn func(hashtags []string, limit int, offset int, apmTransaction *apm.Transaction, forceLog bool) chan HashtagsGetInternalResponseChan
+	GetHashtagsInternalFn func(hashtags []string, omitHashtags []string, limit int, offset int, apmTransaction *apm.Transaction, forceLog bool) chan HashtagsGetInternalResponseChan
 }
 
-func (w *HashtagWrapperMock) GetHashtagsInternal(hashtags []string, limit int, offset int, apmTransaction *apm.Transaction, forceLog bool) chan HashtagsGetInternalResponseChan {
-	return w.GetHashtagsInternalFn(hashtags, limit, offset, apmTransaction, forceLog)
+func (w *HashtagWrapperMock) GetHashtagsInternal(hashtags []string, omitHashtags []string, limit int, offset int, apmTransaction *apm.Transaction, forceLog bool) chan HashtagsGetInternalResponseChan {
+	return w.GetHashtagsInternalFn(hashtags, omitHashtags, limit, offset, apmTransaction, forceLog)
 }
 
 func GetMock() IHashtagWrapper {
