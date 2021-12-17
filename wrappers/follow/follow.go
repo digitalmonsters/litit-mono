b4b5bba33e20b5e58c2b19ff390693eb7c3e99f0
Dariia <grimalskayad@gmail.com>
2021-12-17 15:12:12 +0200

    feat: fix name

--- wrappers/follow/follow.go
+++ wrappers/follow/follow.go
@@ -35,7 +35,7 @@ func NewFollowWrapper(config boilerplate.WrapperConfig) IFollowWrapper {
 		baseWrapper:    wrappers.GetBaseWrapper(),
 		defaultTimeout: timeout,
 		apiUrl:         fmt.Sprintf("%v/rpc", common.StripSlashFromUrl(config.ApiUrl)),
-		serviceName:    "content",
+		serviceName:    "follows",
 	}
 }
 
