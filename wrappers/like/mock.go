e04fa0b8c84c4c9633b50d491bee1c5e61cdfb9c
Dariia <grimalskayad@gmail.com>
2021-12-17 14:46:42 +0200

    feat: follow-wrapper

--- wrappers/like/mock.go
+++ wrappers/like/mock.go
@@ -5,11 +5,17 @@ import "go.elastic.co/apm"
 //goland:noinspection ALL
 type LikeWrapperMock struct {
 	GetLastLikesByUsersFn func(userIds []int64, limitPerUser int, apmTransaction *apm.Transaction, forceLog bool) chan LastLikedByUserResponseChan
+	GetLikeContentUserByContentIdsInternalFn func(contentIds []int64, userId int64, apmTransaction *apm.Transaction, forceLog bool) chan LikeContentUserByContentIdsResponseChan
 }
 
 func (m *LikeWrapperMock) GetLastLikesByUsers(userIds []int64, limitPerUser int, apmTransaction *apm.Transaction, forceLog bool) chan LastLikedByUserResponseChan {
 	return m.GetLastLikesByUsersFn(userIds, limitPerUser, apmTransaction, forceLog)
 }
+
+func (w *LikeWrapperMock) GetLikeContentUserByContentIdsInternal(contentIds []int64, userId int64, apmTransaction *apm.Transaction, forceLog bool) chan LikeContentUserByContentIdsResponseChan {
+	return w.GetLikeContentUserByContentIdsInternalFn(contentIds, userId, apmTransaction, forceLog)
+}
+
 
 func GetMock() ILikeWrapper { // for compiler errors
 	return &LikeWrapperMock{}
--- wrappers/like/structs.go
+++ wrappers/like/structs.go
@@ -16,3 +16,16 @@ type GetLatestLikedByUserRequest struct {
 	LimitPerUser int     `json:"limit_per_user"`
 	UserIds      []int64 `json:"user_ids"`
 }
+
+
+//goland:noinspection GoNameStartsWithPackageName
+type LikeContentUserByContentIdsResponseChan struct {
+	Error *rpc.RpcError  `json:"error"`
+	Data  map[int64]bool `json:"follow_statuses"`
+}
+
+//goland:noinspection GoNameStartsWithPackageName
+type LikeContentUserByContentIdsRequest struct {
+	UserId     int64   `json:"user_id"`
+	ContentIds []int64 `json:"content_ids"`
+}
