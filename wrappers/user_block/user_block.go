f844b7b0e29f29d01ce4afdb2fa49a72dcbd7053
Stas D <iqpirat@gmail.com>
2021-12-20 15:51:31 +0200

    fix: block list

--- wrappers/user_block/user_block.go
+++ wrappers/user_block/user_block.go
@@ -13,6 +13,7 @@ import (
 )
 
 type IUserBlockWrapper interface {
+	GetBlockList(userIds []int64, apmTransaction *apm.Transaction, forceLog bool) chan GetBlockListResponseChan
 	GetUserBlock(blockedTo int64, blockedBy int64, apmTransaction *apm.Transaction, forceLog bool) chan GetUserBlockResponseChan
 }
 
