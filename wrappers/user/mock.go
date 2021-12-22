3f8cf67527f3dbb3d9d30c95250a273089a06859
a-porubai <89394217+a-porubai@users.noreply.github.com>
2021-12-22 14:49:11 +0200

    feat BR-341: get profiles bulk (#58)
    Co-authored-by: Stas Dm <iqpirat@gmail.com>

--- wrappers/user/mock.go
+++ wrappers/user/mock.go
@@ -6,6 +6,7 @@ import "go.elastic.co/apm"
 type UserWrapperMock struct {
 	GetUsersFn       func(userIds []int64, apmTransaction *apm.Transaction, forceLog bool) chan GetUsersResponseChan
 	GetUsersDetailFn func(userIds []int64, apmTransaction *apm.Transaction, forceLog bool) chan GetUsersDetailsResponseChan
+	GetProfileBulkFn func(currentUserId int64, userIds []int64, apmTransaction *apm.Transaction, forceLog bool) chan GetProfileBulkResponseChan
 }
 
 func (m *UserWrapperMock) GetUsers(userIds []int64, apmTransaction *apm.Transaction, forceLog bool) chan GetUsersResponseChan {
@@ -14,6 +15,10 @@ func (m *UserWrapperMock) GetUsers(userIds []int64, apmTransaction *apm.Transact
 
 func (m *UserWrapperMock) GetUsersDetails(userIds []int64, apmTransaction *apm.Transaction, forceLog bool) chan GetUsersDetailsResponseChan {
 	return m.GetUsersDetailFn(userIds, apmTransaction, forceLog)
+}
+
+func (m *UserWrapperMock) GetProfileBulk(currentUserId int64, userIds []int64, apmTransaction *apm.Transaction, forceLog bool) chan GetProfileBulkResponseChan {
+	return m.GetProfileBulkFn(currentUserId, userIds, apmTransaction, forceLog)
 }
 
 func GetMock() IUserWrapper { // for compiler errors
