a17882d416ffb2e9e8d7c67ae7ced78949ccee04
Dariia <grimalskayad@gmail.com>
2022-01-06 12:38:15 +0200

    feat: BR-490 public api rpc -> rest

--- cmd/api/public_api.go
+++ cmd/api/public_api.go
@@ -1,59 +1,106 @@
 package api
 
 import (
-	"encoding/json"
 	"github.com/digitalmonsters/go-common/common"
 	"github.com/digitalmonsters/go-common/error_codes"
 	"github.com/digitalmonsters/go-common/router"
 	"github.com/digitalmonsters/go-common/swagger"
 	"github.com/digitalmonsters/music/pkg/database"
 	"github.com/digitalmonsters/music/pkg/favorites"
+	"github.com/digitalmonsters/music/utils"
+	"github.com/pkg/errors"
+	"net/http"
 )
 
 func InitPublicApi(httpRouter *router.HttpRouter, apiDef map[string]swagger.ApiDescription) error {
-	if err := httpRouter.RegisterRpcCommand(router.NewCommand("AddToFavorites", func(request []byte, executionData router.MethodExecutionData) (interface{}, *error_codes.ErrorWithCode) {
-		var req favorites.AddToFavoritesRequest
+	if err := httpRouter.RegisterRestCmd(router.NewRestCommand(func(request []byte,
+		executionData router.MethodExecutionData) (interface{}, *error_codes.ErrorWithCode) {
+		userId := executionData.UserId
 
-		if err := json.Unmarshal(request, &req); err != nil {
-			return nil, error_codes.NewErrorWithCodeRef(err, error_codes.GenericMappingError)
+		if userId <= 0 {
+			return nil, error_codes.NewErrorWithCodeRef(errors.New("invalid user_id"), error_codes.GenericValidationError)
+		}
+
+		songId := utils.ExtractString(executionData.GetUserValue, "song_id", "")
+
+		if len(songId) <= 0 {
+			return nil, error_codes.NewErrorWithCodeRef(errors.New("invalid song_id"), error_codes.GenericValidationError)
+		}
+
+		req := favorites.AddToFavoritesRequest{
+			UserId: userId,
+			SongId: songId,
 		}
 
 		if err := favorites.AddToFavorites(req, database.GetDb(database.DbTypeMaster).WithContext(executionData.Context)); err != nil {
 			return nil, error_codes.NewErrorWithCodeRef(err, error_codes.GenericServerError)
+		} else {
+			return successResponse{
+				Success: true,
+			}, nil
 		}
-
-		return "ok", nil
-	}, common.AccessLevelRead, false, true)); err != nil {
+	}, "/favorites/add/{song_id}", http.MethodPost, common.AccessLevelPublic, true, false)); err != nil {
 		return err
 	}
 
-	if err := httpRouter.RegisterRpcCommand(router.NewCommand("RemoveFromFavorites", func(request []byte, executionData router.MethodExecutionData) (interface{}, *error_codes.ErrorWithCode) {
-		var req favorites.RemoveFromFavoritesRequest
+	if err := httpRouter.RegisterRestCmd(router.NewRestCommand(func(request []byte,
+		executionData router.MethodExecutionData) (interface{}, *error_codes.ErrorWithCode) {
+		userId := executionData.UserId
 
-		if err := json.Unmarshal(request, &req); err != nil {
-			return nil, error_codes.NewErrorWithCodeRef(err, error_codes.GenericMappingError)
+		if userId <= 0 {
+			return nil, error_codes.NewErrorWithCodeRef(errors.New("invalid user_id"), error_codes.GenericValidationError)
+		}
+
+		songId := utils.ExtractString(executionData.GetUserValue, "song_id", "")
+
+		if len(songId) <= 0 {
+			return nil, error_codes.NewErrorWithCodeRef(errors.New("invalid song_id"), error_codes.GenericValidationError)
+		}
+
+		req := favorites.RemoveFromFavoritesRequest{
+			UserId: userId,
+			SongId: songId,
 		}
 
 		if err := favorites.RemoveFromFavorites(req, database.GetDb(database.DbTypeMaster).WithContext(executionData.Context)); err != nil {
 			return nil, error_codes.NewErrorWithCodeRef(err, error_codes.GenericServerError)
+		} else {
+			return successResponse{
+				Success: true,
+			}, nil
 		}
-
-		return "ok", nil
-	}, common.AccessLevelRead, false, true)); err != nil {
+	}, "/favorites/remove/{song_id}", http.MethodPost, common.AccessLevelPublic, true, false)); err != nil {
 		return err
 	}
 
-	apiDef["AddToFavorites"] = swagger.ApiDescription{
-		Request:  favorites.AddToFavoritesRequest{},
-		Response: nil,
-		Tags:     []string{"favorites", "user"},
+	apiDef["/favorites/add/{song_id}"] = swagger.ApiDescription{
+		AdditionalSwaggerParameters: []swagger.ParameterDescription{
+			{
+				Name:        "song_id",
+				In:          swagger.ParameterInPath,
+				Description: "song_id",
+				Required:    true,
+				Type:        "string",
+			},
+		},
+		Response:          successResponse{},
+		MethodDescription: "add song ro favorites",
+		Tags:              []string{"favorites", "public"},
 	}
 
-	apiDef["RemoveFromFavorites"] = swagger.ApiDescription{
-		Request:  favorites.RemoveFromFavoritesRequest{},
-		Response: nil,
-		Tags:     []string{"favorites", "user"},
+	apiDef["/favorites/remove/{song_id}"] = swagger.ApiDescription{
+		AdditionalSwaggerParameters: []swagger.ParameterDescription{
+			{
+				Name:        "song_id",
+				In:          swagger.ParameterInPath,
+				Description: "song_id",
+				Required:    true,
+				Type:        "string",
+			},
+		},
+		Response:          successResponse{},
+		MethodDescription: "remove song from favorites",
+		Tags:              []string{"favorites", "public"},
 	}
-
 	return nil
 }
