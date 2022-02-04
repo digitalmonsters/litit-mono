96ec399740a7c71156ace772e43742d4d1eb9c6b
Stas D <iqpirat@gmail.com>
2022-02-04 11:17:54 +0200

    feat: db config

--- boilerplate/config_reader.go
+++ boilerplate/config_reader.go
@@ -88,11 +88,15 @@ type ApmConfig struct {
 }
 
 type DbConfig struct {
-	Host     string `json:"Host"`
-	Port     int    `json:"Port"`
-	Db       string `json:"Db"`
-	User     string `json:"User"`
-	Password string `json:"Password"`
+	Host                     string `json:"Host"`
+	Port                     int    `json:"Port"`
+	Db                       string `json:"Db"`
+	User                     string `json:"User"`
+	Password                 string `json:"Password"`
+	MaxIdleConnections       int    `json:"MaxIdleConnections"`
+	MaxConnectionLifetimeSec int    `json:"MaxConnectionLifetimeSec"`
+	MaxOpenConnections       int    `json:"MaxOpenConnections"`
+	MaxConnectionIdleSec     int    `json:"MaxConnectionIdleSec"`
 }
 
 type RedisConfig struct {
