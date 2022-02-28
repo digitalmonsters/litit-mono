24a04e2dca246b42e1e52c07d4f65f1c50288541
Stas Dm <iqpirat@gmail.com>
2022-02-28 18:56:46 +0200

    feat: additinal event

--- eventsourcing/views.go
+++ eventsourcing/views.go
@@ -14,3 +14,13 @@ type UserTotalWatchTimeEvent struct { // local.total_watch_time
 func (v UserTotalWatchTimeEvent) GetPublishKey() string {
 	return fmt.Sprintf("%v", v.UserId)
 }
+
+type UserTodayWatchTimeEvent struct { // local.today_watch_time
+	UserId              int64     `json:"user_id"`
+	TotalTodayWatchTime int64     `json:"total_today_watch_time"`
+	CreatedAt           time.Time `json:"created_at"`
+}
+
+func (v UserTodayWatchTimeEvent) GetPublishKey() string {
+	return fmt.Sprintf("%v", v.UserId)
+}
