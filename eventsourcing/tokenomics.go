f3867cb3252fe42b4145d4544f4bb358e5d6b9a4
Stas Dm <iqpirat@gmail.com>
2022-03-02 17:01:32 +0200

    feat: user balance change

---
+++ eventsourcing/tokenomics.go
@@ -0,0 +1,24 @@
+package eventsourcing
+
+import (
+	"fmt"
+	"github.com/shopspring/decimal"
+)
+
+type UserBalanceChangeEvent struct {
+	UserId int64 `json:"user_id"`
+
+	TotalTokens decimal.Decimal `json:"total_tokens"`
+	TotalPoints decimal.Decimal `json:"total_points"`
+
+	CurrentPoints decimal.Decimal `json:"current_points"`
+	CurrentTokens decimal.Decimal `json:"current_tokens"`
+	CurrentRate   decimal.Decimal `json:"current_rate"`
+
+	VaultPoints        decimal.Decimal `json:"vault_points"`
+	AllTimeVaultPoints decimal.Decimal `json:"all_time_vault_points"`
+}
+
+func (u UserBalanceChangeEvent) GetPublishKey() string {
+	return fmt.Sprintf("%v", u.UserId)
+}
