43ecd5b4ea8f39d79e7658540274b29cfd5e136b
vladislav-siz-bz <vladislav@siz.bz>
2022-03-11 13:04:59 +0200

    Fix platform type

--- pkg/database/device.go
+++ pkg/database/device.go
@@ -1,16 +1,16 @@
 package database
 
 import (
-	"github.com/digitalmonsters/go-common/wrappers/notification_gateway"
+	"github.com/digitalmonsters/go-common/common"
 	"github.com/google/uuid"
 )
 
 type Device struct {
 	Id        uuid.UUID
-	UserId    int64                           `gorm:"column:userId"`
-	DeviceId  string                          `gorm:"column:deviceId"`
-	PushToken string                          `gorm:"column:pushToken"`
-	Platform  notification_gateway.DeviceType `json:"platform"`
+	UserId    int64             `gorm:"column:userId"`
+	DeviceId  string            `gorm:"column:deviceId"`
+	PushToken string            `gorm:"column:pushToken"`
+	Platform  common.DeviceType `json:"platform"`
 }
 
 func (Device) TableName() string {
