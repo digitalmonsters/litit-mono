c416a49e6bf4cf3b0317f0d14302c6221d8d7877
Stas Dm <iqpirat@gmail.com>
2022-03-18 11:50:21 +0200

    fix: listener

--- cmd/main.go
+++ cmd/main.go
@@ -59,7 +59,7 @@ func main() {
 	voteNotifier := vote2.NewNotifier(time.Duration(cfg.NotifierVoteConfig.PollTimeMs)*time.Millisecond,
 		ctx, eventsourcing.NewKafkaEventPublisher(*cfg.KafkaWriter, cfg.NotifierVoteConfig.KafkaTopic), true)
 
-	user_consumer.InitListener(ctx, cfg.UserListener)
+	user_consumer.InitListener(ctx, cfg.UserListener).ListenAsync()
 
 	if err := comments.Init(fastHttpRouter, db, userWrapper, contentWrapper, userBlockWrapper, apiDef, commentNotifier,
 		contentCommentsNotifier, userCommentsNotifier); err != nil {
