ac8347085dffd8f6b2da3d95d477f39cbae295fa
Stas Dmytryshyn <iqpirat@gmail.com>
2022-03-24 10:56:07 +0300

    feat: remove extra properties (#161)
    * feat: remove extra properties
    
    * feat: codeowners

--- eventsourcing/comments.go
+++ eventsourcing/comments.go
@@ -7,22 +7,17 @@ import (
 )
 
 type Comment struct {
-	Id              int64       `json:"id"`
-	AuthorId        int64       `json:"author_id"`
-	NumReplies      int64       `json:"num_replies"`
-	NumUpvotes      int64       `json:"num_upvotes"`
-	NumDownvotes    int64       `json:"num_downvotes"`
-	CreatedAt       time.Time   `json:"created_at"`
-	Active          bool        `json:"active"`
-	Comment         string      `json:"comment"`
-	ContentId       null.Int    `json:"content_id"`
-	ParentId        null.Int    `json:"parent_id"`
-	ParentAuthorId  null.Int    `json:"parent_author_id"`
-	Width           null.Int    `json:"width"`
-	Height          null.Int    `json:"height"`
-	VideoId         null.String `json:"video_id"`
-	ContentAuthorId null.Int    `json:"content_author_id"`
-	ProfileId       null.Int    `json:"profile_id"`
+	Id           int64     `json:"id"`
+	AuthorId     int64     `json:"author_id"`
+	NumReplies   int64     `json:"num_replies"`
+	NumUpvotes   int64     `json:"num_upvotes"`
+	NumDownvotes int64     `json:"num_downvotes"`
+	CreatedAt    time.Time `json:"created_at"`
+	Active       bool      `json:"active"`
+	Comment      string    `json:"comment"`
+	ContentId    null.Int  `json:"content_id"`
+	ParentId     null.Int  `json:"parent_id"`
+	ProfileId    null.Int  `json:"profile_id"`
 	BaseChangeEvent
 }
 
