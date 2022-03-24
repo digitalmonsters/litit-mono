647b7610ccca7256f9c7bd455bd4c3b028479053
Dariia <grimalskayad@gmail.com>
2022-03-24 12:31:35 +0200

    feat: no following add offset

--- wrappers/content/mock.go
+++ wrappers/content/mock.go
@@ -4,15 +4,15 @@ import "go.elastic.co/apm"
 
 type ContentWrapperMock struct {
 	GetInternalFn             func(contentIds []int64, includeDeleted bool, apmTransaction *apm.Transaction, forceLog bool) chan ContentGetInternalResponseChan
-	GetTopNotFollowingUsersFn func(userId int64, limit int, apmTransaction *apm.Transaction, forceLog bool) chan GetTopNotFollowingUsersResponseChan
+	GetTopNotFollowingUsersFn func(userId int64, limit int, offset int, apmTransaction *apm.Transaction, forceLog bool) chan GetTopNotFollowingUsersResponseChan
 }
 
 func (w *ContentWrapperMock) GetInternal(contentIds []int64, includeDeleted bool, apmTransaction *apm.Transaction, forceLog bool) chan ContentGetInternalResponseChan {
 	return w.GetInternalFn(contentIds, includeDeleted, apmTransaction, forceLog)
 }
 
-func (w *ContentWrapperMock) GetTopNotFollowingUsers(userId int64, limit int, apmTransaction *apm.Transaction, forceLog bool) chan GetTopNotFollowingUsersResponseChan {
-	return w.GetTopNotFollowingUsersFn(userId, limit, apmTransaction, forceLog)
+func (w *ContentWrapperMock) GetTopNotFollowingUsers(userId int64, limit int, offset int, apmTransaction *apm.Transaction, forceLog bool) chan GetTopNotFollowingUsersResponseChan {
+	return w.GetTopNotFollowingUsersFn(userId, limit, offset, apmTransaction, forceLog)
 }
 
 func GetMock() IContentWrapper { // for compiler errors
