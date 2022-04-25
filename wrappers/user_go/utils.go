8c6d6409a6b25ba38b41817a765f1fd89fd13615
dariiamoisol <94370133+dariiamoisol@users.noreply.github.com>
2022-04-25 11:29:28 +0300

    feat: BR-1379 (#213)
    Co-authored-by: Dariia <grimalskayad@gmail.com>

--- wrappers/user_go/utils.go
+++ wrappers/user_go/utils.go
@@ -1,7 +1,5 @@
 package user_go
 
-import "fmt"
-
 func getFirstAndLastNameWithPrivacy(status NamePrivacyStatus, firstName string, lastName string, userName string) (string, string) {
 	if status == NamePrivacyStatusAllHidden || status == NamePrivacyStatusFirstNameHidden ||
 		status == NamePrivacyStatusLastNameHidden || (len(firstName) == 0 && len(lastName) == 0) {
@@ -12,9 +10,5 @@ func getFirstAndLastNameWithPrivacy(status NamePrivacyStatus, firstName string, 
 }
 
 func formatUserName(userName string) string {
-	if len(userName) == 0 {
-		return ""
-	}
-
-	return fmt.Sprintf("@%v", userName)
+	return userName
 }
