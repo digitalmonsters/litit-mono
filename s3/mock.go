3c5cd0c1983105593bcf1ee0a181f7f003be4392
Stas Dm <iqpirat@gmail.com>
2022-05-04 10:43:56 +0300

    Revert "feat: change s3"
    This reverts commit 4562220449611ffefc3b8ec9dfdfea5d0a0ce6a3.

--- s3/mock.go
+++ s3/mock.go
@@ -1,13 +1,12 @@
 package s3
 
 import (
-	"github.com/aws/aws-sdk-go/service/s3"
 	"time"
 )
 
 type UploaderMock struct {
 	GetObjectSignedUrlFn func(path string, urlExpiration time.Duration) (string, error)
-	PutObjectSignedUrlFn func(req s3.PutObjectInput, expiration time.Duration) (string, error)
+	PutObjectSignedUrlFn func(path string, urlExpiration time.Duration) (string, error)
 	GetObjectSizeFn      func(path string) (int64, error)
 	UploadObjectFn       func(path string, data []byte, contentType string) error
 }
@@ -15,8 +14,8 @@ type UploaderMock struct {
 func (u *UploaderMock) GetObjectSignedUrl(path string, urlExpiration time.Duration) (string, error) {
 	return u.GetObjectSignedUrlFn(path, urlExpiration)
 }
-func (u *UploaderMock) PutObjectSignedUrl(req s3.PutObjectInput, expiration time.Duration) (string, error) {
-	return u.PutObjectSignedUrlFn(req, expiration)
+func (u *UploaderMock) PutObjectSignedUrl(path string, urlExpiration time.Duration) (string, error) {
+	return u.PutObjectSignedUrlFn(path, urlExpiration)
 }
 func (u *UploaderMock) GetObjectSize(path string) (int64, error) {
 	return u.GetObjectSizeFn(path)
@@ -26,5 +25,5 @@ func (u *UploaderMock) UploadObject(path string, data []byte, contentType string
 }
 
 func GetMock() IUploader { // for compiler errors
-	return &UploaderMock{}
+	return &Uploader{}
 }
