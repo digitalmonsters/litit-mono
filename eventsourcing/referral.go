3e5d631868efc6144d571c52957ca7a8612f658b
dariiamoisol <94370133+dariiamoisol@users.noreply.github.com>
2022-05-04 19:22:50 +0300

    Feature/br 1352 add grand referral to user event (#231)
    * feat: BR-1352 add grand referral to user event
    
    * featL BR-1352 add grand referrer id to referrer verified event
    
    * feat: BR-1352
    
    * feat: BR-1352 no breaking changes
    
    Co-authored-by: Dariia <grimalskayad@gmail.com>

--- eventsourcing/referral.go
+++ eventsourcing/referral.go
@@ -6,10 +6,12 @@ import (
 )
 
 type ReferrerVerifiedEvent struct {
-	UserId             int64                 `json:"user_id"`
-	ReferrerId         int64                 `json:"referrer_id"`
-	ReferredByType     common.VerifiedByType `json:"referred_by_type"`
-	IsVerifiedReferrer bool                  `json:"is_verified_referrer"`
+	UserId                  int64                 `json:"user_id"`
+	ReferrerId              int64                 `json:"referrer_id"`
+	ReferredByType          common.VerifiedByType `json:"referred_by_type"`
+	IsVerifiedReferrer      bool                  `json:"is_verified_referrer"`
+	GrandReferrerId         int64                 `json:"grand_referrer_id"`
+	IsVerifiedGrandReferrer bool                  `json:"is_verified_grand_referrer"`
 }
 
 func (c ReferrerVerifiedEvent) GetPublishKey() string {
