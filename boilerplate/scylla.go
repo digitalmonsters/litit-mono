12e7049854be1947622e856c913c5cb0bb5509dc
Dmitry Golodov <94372291+dimitriy5656@users.noreply.github.com>
2022-06-03 17:37:16 +0300

    BR-1751: EnsureKeyspaceExists in GetScyllaCluster (#275)
    * BR-1751: EnsureKeyspaceExists in GetScyllaCluster
    
    * remove boilerplate.Local check

--- boilerplate/scylla.go
+++ boilerplate/scylla.go
@@ -1,33 +1,89 @@
 package boilerplate
 
 import (
+	"fmt"
 	"github.com/gocql/gocql"
+	"github.com/pkg/errors"
 	"time"
 )
 
 func GetScyllaCluster(config ScyllaConfiguration) *gocql.ClusterConfig {
-	//observer := apmgocql.NewObserver()
-	scyllaCluster := gocql.NewCluster(SplitHostsToSlice(config.Hosts)...)
-	scyllaCluster.Keyspace = config.Keyspace
+	oldKeyspace := config.Keyspace
 
-	scyllaCluster.Authenticator = gocql.PasswordAuthenticator{
-		Username: config.UserName,
-		Password: config.Password,
+	if oldKeyspace != "system" {
+		if err := EnsureKeyspaceExists(config, oldKeyspace); err != nil {
+			panic(errors.WithStack(err))
+		}
 	}
 
-	timeout := 10 * time.Second
+	cluster, _, err := GetScyllaClusterInternal(config)
+	if err != nil {
+		panic(errors.WithStack(err))
+	}
+
+	return cluster
+}
+
+func EnsureKeyspaceExists(config ScyllaConfiguration, targetKeyspace string) error {
+	config.Keyspace = "system"
+
+	_, ses, err := GetScyllaClusterInternal(config)
+
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	defer ses.Close()
+
+	iter := ses.Query(fmt.Sprintf("SELECT keyspace_name FROM system_schema.keyspaces WHERE keyspace_name='%v'", targetKeyspace)).Iter()
+
+	var keyspaceName string
+
+	for iter.Scan(&keyspaceName) {
+		break
+	}
+
+	if err := iter.Close(); err != nil {
+		return err
+	}
+
+	if keyspaceName == targetKeyspace {
+		return nil
+	}
+
+	if err = ses.Query(fmt.Sprintf(`CREATE KEYSPACE IF NOT EXISTS %v WITH replication = {
+			'class' : 'SimpleStrategy',
+			'replication_factor' : 1
+		}`, targetKeyspace)).Exec(); err != nil {
+		return err
+	}
+
+	return err
+}
+
+func GetScyllaClusterInternal(config ScyllaConfiguration) (*gocql.ClusterConfig, *gocql.Session, error) {
+	newCluster := gocql.NewCluster(SplitHostsToSlice(config.Hosts)...)
+
+	timeout := 20 * time.Second
+
 	if config.TimeoutSeconds > 0 {
 		timeout = time.Duration(config.TimeoutSeconds) * time.Second
 	}
 
-	scyllaCluster.Timeout = timeout
-	scyllaCluster.MaxPreparedStmts = config.MaxPreparedStmts
-	scyllaCluster.NumConns = config.NumConns
-	scyllaCluster.MaxRoutingKeyInfo = config.MaxRoutingKeyInfo
-	scyllaCluster.PageSize = config.PageSize
+	newCluster.Timeout = timeout
+	//newCluster.ConnectTimeout = timeout
+	newCluster.Authenticator = gocql.PasswordAuthenticator{
+		Username: config.UserName,
+		Password: config.Password,
+	}
 
-	//scyllaCluster.QueryObserver = observer
-	//scyllaCluster.BatchObserver = observer
+	newCluster.MaxPreparedStmts = config.MaxPreparedStmts
+	newCluster.NumConns = config.NumConns
+	newCluster.MaxRoutingKeyInfo = config.MaxRoutingKeyInfo
+	newCluster.PageSize = config.PageSize
+	newCluster.Keyspace = config.Keyspace
 
-	return scyllaCluster
+	newSession, err := newCluster.CreateSession()
+
+	return newCluster, newSession, err
 }
--- boilerplate_testing/scylla.go
+++ boilerplate_testing/scylla.go
@@ -12,7 +12,6 @@ import (
 	"io/fs"
 	"os"
 	"testing"
-	"time"
 )
 
 func GetScyllaTestCluster(config *boilerplate.ScyllaConfiguration) (*gocql.ClusterConfig, *gocql.Session, error) {
@@ -22,12 +21,12 @@ func GetScyllaTestCluster(config *boilerplate.ScyllaConfiguration) (*gocql.Clust
 	oldKeyspace := config.Keyspace
 
 	if oldKeyspace != "system" {
-		if err := ensureKeyspaceExists(*config, oldKeyspace); err != nil {
+		if err := boilerplate.EnsureKeyspaceExists(*config, oldKeyspace); err != nil {
 			return nil, nil, errors.WithStack(err)
 		}
 	}
 
-	return getScyllaClusterInternal(*config)
+	return boilerplate.GetScyllaClusterInternal(*config)
 }
 
 func GetScyllaCiKeyspaceName() string {
@@ -102,68 +101,4 @@ func innerScyllaFlushTables(t *testing.T, session *gocql.Session, keyspace strin
 
 	}
 	return nil
-}
-
-func ensureKeyspaceExists(config boilerplate.ScyllaConfiguration, targetKeyspace string) error {
-	config.Keyspace = "system"
-
-	_, ses, err := getScyllaClusterInternal(config)
-
-	if err != nil {
-		return errors.WithStack(err)
-	}
-
-	defer ses.Close()
-
-	iter := ses.Query(fmt.Sprintf("SELECT keyspace_name FROM system_schema.keyspaces WHERE keyspace_name='%v'", targetKeyspace)).Iter()
-
-	var keyspaceName string
-
-	for iter.Scan(&keyspaceName) {
-		break
-	}
-
-	if err := iter.Close(); err != nil {
-		return err
-	}
-
-	if keyspaceName == targetKeyspace {
-		return nil
-	}
-
-	if err = ses.Query(fmt.Sprintf(`CREATE KEYSPACE IF NOT EXISTS %v WITH replication = {
-			'class' : 'SimpleStrategy',
-			'replication_factor' : 1
-		}`, targetKeyspace)).Exec(); err != nil {
-		return err
-	}
-
-	return err
-}
-
-func getScyllaClusterInternal(config boilerplate.ScyllaConfiguration) (*gocql.ClusterConfig, *gocql.Session, error) {
-	newCluster := gocql.NewCluster(boilerplate.SplitHostsToSlice(config.Hosts)...)
-
-	timeout := 20 * time.Second
-
-	if config.TimeoutSeconds > 0 {
-		timeout = time.Duration(config.TimeoutSeconds) * time.Second
-	}
-
-	newCluster.Timeout = timeout
-	//newCluster.ConnectTimeout = timeout
-	newCluster.Authenticator = gocql.PasswordAuthenticator{
-		Username: config.UserName,
-		Password: config.Password,
-	}
-
-	newCluster.MaxPreparedStmts = config.MaxPreparedStmts
-	newCluster.NumConns = config.NumConns
-	newCluster.MaxRoutingKeyInfo = config.MaxRoutingKeyInfo
-	newCluster.PageSize = config.PageSize
-	newCluster.Keyspace = config.Keyspace
-
-	newSession, err := newCluster.CreateSession()
-
-	return newCluster, newSession, err
 }
