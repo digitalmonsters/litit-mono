a26ae269d989a1fd5c5c8db681751ac5664b1712
Dmitry Golodov <94372291+dimitriy5656@users.noreply.github.com>
2022-06-10 14:00:01 +0300

    BR-1758: add CustomData to AdminPushMessageEventData (#282)

--- eventsourcing/admin_push.go
+++ eventsourcing/admin_push.go
@@ -3,9 +3,10 @@ package eventsourcing
 import "fmt"
 
 type AdminPushMessageEventData struct {
-	UserId  int64  `json:"user_id"`
-	Title   string `json:"title"`
-	Message string `json:"message"`
+	UserId     int64                  `json:"user_id"`
+	Title      string                 `json:"title"`
+	Message    string                 `json:"message"`
+	CustomData map[string]interface{} `json:"custom_data"`
 }
 
 func (t AdminPushMessageEventData) GetPublishKey() string {
