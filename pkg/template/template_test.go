622f5a065c16f549021c6d59faa6126f783fd780
dmitriy.g <d.holodov@mediatech.ai>
2022-06-15 18:31:21 +0300

    fix tests

--- pkg/template/template_test.go
+++ pkg/template/template_test.go
@@ -53,7 +53,7 @@ func TestService_EditTemplate(t *testing.T) {
 
 	a := assert.New(t)
 
-	a.True(strings.Contains("", "1"))
+	a.True(strings.Contains(template.Kind, "1"))
 }
 
 func TestService_ListTemplates(t *testing.T) {
@@ -86,10 +86,10 @@ func TestService_ListTemplates(t *testing.T) {
 	}
 
 	resp, err := templateService.ListTemplates(ListTemplatesRequest{
-		Id:       "template1",
-		Title:    "template1",
-		Body:     "template1",
-		Headline: "template1",
+		Id: "template1",
+		//Title:    "template1",
+		//Body:     "template1",
+		//Headline: "template1",
 		Kind:     "template1",
 		Route:    "template1",
 		ImageUrl: "template1",
