611e782f9b529a4f03c5c4d980a9b426adecd44c
Dmitry Golodov <94372291+dimitriy5656@users.noreply.github.com>
2022-06-21 21:52:50 +0300

    Feature/br 1937 (#199)
    * BR-1937: add universal time round to minutes, change PushNotificationDeadlineKeyMinutes, PushNotificationDeadlineMinutes to 720
    
    * update cron
    
    * use jobber for migrator
    
    * fix deadlines
    
    * fix defer
    
    * add additional logs
    
    * use cursor paging
    
    * fix lint

--- pkg/migrator/migrator.go
+++ pkg/migrator/migrator.go
@@ -13,6 +13,7 @@ import (
 	"github.com/digitalmonsters/notification-handler/pkg/database/scylla"
 	"github.com/digitalmonsters/notification-handler/pkg/renderer"
 	"github.com/gocql/gocql"
+	"github.com/pilagod/gorm-cursor-paginator/v2/paginator"
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -64,18 +65,6 @@ func MigrateNotificationsToScylla(ctx context.Context) error {
 	db := database.GetDbWithContext(database.DbTypeReadonly, ctx)
 	timeNow := time.Now().UTC()
 
-	var dbNotifications []database.Notification
-	if err := db.Where("created_at >= ? and created_at <= ?", timeNow.Add(-3*24*30*time.Hour),
-		time.Date(2022, 6, 18, 10, 25, 0, 0, time.UTC)).Order("created_at asc").
-		Find(&dbNotifications).Error; err != nil {
-		return errors.WithStack(err)
-	}
-
-	if len(dbNotifications) == 0 {
-		logger.Info().Msg("[MigrateNotificationsToScylla] dbNotification len 0")
-		return nil
-	}
-
 	var templates []database.RenderTemplate
 	if err := db.Find(&templates).Error; err != nil {
 		return errors.WithStack(err)
@@ -97,191 +86,243 @@ func MigrateNotificationsToScylla(ctx context.Context) error {
 	groupedNotifications := make(map[int64]map[string]map[int64]scylla.Notification)
 	var scyllaNotificationsToUpdate []scylla.Notification
 
-	logger.Info().Msgf("[MigrateNotificationsToScylla] before dbNotifications iterations, len %v", len(scyllaNotificationsToUpdate))
-
-	for _, dbNotification := range dbNotifications {
-		eventTypes := database.GetNotificationTemplates(dbNotification.Type)
-
-		if len(eventTypes) == 0 {
-			logger.Info().Msgf("[MigrateNotificationsToScylla] eventTypes for \"%v\" not found", dbNotification.Type)
-			continue
+	after := ""
+	isFirst := true
+	for {
+		if !isFirst && len(after) == 0 {
+			break
 		}
 
-		eventType := ""
+		isFirst = false
 
-		if len(eventTypes) > 1 {
-			switch dbNotification.Type {
-			case "push.paid_views.first":
-				if strings.Contains(dbNotification.Title, "Create your account") {
-					eventType = "first_guest_x_paid_views"
-				} else {
-					eventType = "first_x_paid_views"
-				}
-			case "push.guest.after_install":
-				if strings.Contains(dbNotification.Message, "Complete your account creation") {
-					eventType = "guest_after_install_first_push"
-				} else if strings.Contains(dbNotification.Message, "On Lit.it the more viral videos you watch the more your earn") {
-					eventType = "guest_after_install_second_push"
-				} else {
-					eventType = "guest_after_install_third_push"
-				}
-			case "push.user.after_signup":
-				if strings.Contains(dbNotification.Message, "Few things you need to know about Lit.it") {
-					eventType = "user_after_signup_first_push"
-				} else if strings.Contains(dbNotification.Message, "Check who earned the most for inviting friends to Lit.it") {
-					eventType = "user_after_signup_second_push"
-				} else if strings.Contains(dbNotification.Message, "Who earned the most LIT points? Check this out") {
-					eventType = "user_after_signup_third_push"
-				} else if strings.Contains(dbNotification.Message, "Check out TOP viral videos on Lit.it & earn LIT points") {
-					eventType = "user_after_signup_fourth_push"
-				} else {
-					eventType = "user_after_signup_fifth_push"
-				}
-			case "push.comment.vote":
-				if strings.Contains(dbNotification.Message, "disliked your comment") {
-					eventType = "comment_vote_dislike"
-				} else {
-					eventType = "comment_vote_like"
-				}
-			case "push.kyc.status":
-				if strings.Contains(dbNotification.Message, "Your identity verification has been approved") {
-					eventType = "kyc_status_verified"
-				} else {
-					eventType = "kyc_status_rejected"
-				}
-			}
-		} else {
-			eventType = eventTypes[0]
-		}
-
-		if len(eventType) == 0 {
-			logger.Info().Msgf("[MigrateNotificationsToScylla] eventTypes for \"%v\" not found is switch", dbNotification.Type)
-			continue
-		}
-
-		template, ok := templatesMap[eventType]
-		if !ok {
-			logger.Info().Msgf("[MigrateNotificationsToScylla] template for \"%v\" not found", eventType)
-			continue
-		}
-
-		logger.Info().Msgf("[MigrateNotificationsToScylla] eventType \"%v\" parsed for notification \"%v\"", eventType, dbNotification.Id.String())
-
-		customDataMarshalled, _ := json.Marshal(dbNotification.CustomData)
-		renderingVariablesMarshalled, _ := json.Marshal(dbNotification.RenderingVariables)
-		notificationMarshalled, _ := json.Marshal(dbNotification)
-
-		scyllaNotification := scylla.Notification{
-			UserId:             dbNotification.UserId,
-			EventType:          eventType,
-			CreatedAt:          dbNotification.CreatedAt,
-			EntityId:           dbNotification.UserId,
-			CustomData:         string(customDataMarshalled),
-			RenderingVariables: string(renderingVariablesMarshalled),
-			NotificationInfo:   string(notificationMarshalled),
-			Kind:               template.Kind,
-			NotificationsCount: 1,
-			Title:              dbNotification.Title,
-			Body:               dbNotification.Message,
-		}
-
-		switch eventType {
-		case "comment_reply":
-			scyllaNotification.Kind = "default"
-			scyllaNotification.RelatedEntityId = dbNotification.RelatedUserId.ValueOrZero()
-
-			if dbNotification.Comment != nil {
-				scyllaNotification.EntityId = dbNotification.Comment.ParentId.ValueOrZero()
-			}
-		case "comment_content_resource_create":
-			scyllaNotification.Kind = "default"
-			scyllaNotification.EntityId = dbNotification.ContentId.ValueOrZero()
-			scyllaNotification.RelatedEntityId = dbNotification.RelatedUserId.ValueOrZero()
-		case "comment_profile_resource_create":
-			scyllaNotification.Kind = "default"
-			scyllaNotification.EntityId = dbNotification.RelatedUserId.ValueOrZero()
-		case "spot_upload":
-			fallthrough
-		case "content_upload":
-			scyllaNotification.Kind = "default"
-			scyllaNotification.EntityId = dbNotification.ContentId.ValueOrZero()
-			scyllaNotification.RelatedEntityId = dbNotification.UserId
-		case "creator_status_rejected":
-			fallthrough
-		case "creator_status_approved":
-			fallthrough
-		case "creator_status_pending":
-			scyllaNotification.Kind = "content_creator"
-			scyllaNotification.EntityId = dbNotification.UserId
-		case "follow":
-			scyllaNotification.Kind = "user_follow"
-			scyllaNotification.EntityId = dbNotification.RelatedUserId.ValueOrZero()
-		case "kyc_status_verified":
-			fallthrough
-		case "kyc_status_rejected":
-			scyllaNotification.Kind = "default"
-			scyllaNotification.EntityId = dbNotification.UserId
-		case "content_like":
-			scyllaNotification.Kind = "default"
-			scyllaNotification.EntityId = dbNotification.ContentId.ValueOrZero()
-			scyllaNotification.RelatedEntityId = dbNotification.RelatedUserId.ValueOrZero()
-		case "push_admin":
-			scyllaNotification.Kind = "default"
-			scyllaNotification.EntityId = dbNotification.UserId
-		case "tip":
-			fallthrough
-		case "bonus_time":
-			fallthrough
-		case "bonus_followers":
-			scyllaNotification.Kind = "default"
-			scyllaNotification.EntityId = dbNotification.RelatedUserId.ValueOrZero()
-		case "comment_vote_dislike":
-			fallthrough
-		case "comment_vote_like":
-			scyllaNotification.Kind = "default"
-			scyllaNotification.EntityId = dbNotification.CommentId.ValueOrZero()
-			scyllaNotification.RelatedEntityId = dbNotification.RelatedUserId.ValueOrZero()
-		}
-
-		if template.IsGrouped {
-			groupedNotification, ok := groupedNotifications[dbNotification.UserId]
-			if !ok {
-				groupedNotifications[dbNotification.UserId] = map[string]map[int64]scylla.Notification{
-					eventType: {
-						scyllaNotification.EntityId: scyllaNotification,
+		p := paginator.New(
+			&paginator.Config{
+				Rules: []paginator.Rule{
+					{
+						Key:   "UserId",
+						Order: paginator.ASC,
 					},
+					{
+						Key:   "CreatedAt",
+						Order: paginator.ASC,
+					},
+				},
+				Limit: 1000,
+			},
+		)
+
+		if len(after) > 0 {
+			p.SetAfterCursor(after)
+		}
+
+		var dbNotifications []database.Notification
+		query := db.Where("created_at >= ? and created_at <= ?", timeNow.Add(-3*24*30*time.Hour),
+			time.Date(2022, 6, 18, 10, 25, 0, 0, time.UTC))
+		result, cursor, err := p.Paginate(query, &dbNotifications)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+
+		if result.Error != nil {
+			return errors.WithStack(result.Error)
+		}
+
+		if len(dbNotifications) == 0 {
+			break
+		}
+
+		if cursor.After != nil {
+			after = *cursor.After
+		} else {
+			after = ""
+		}
+
+		logger.Info().Msgf("[MigrateNotificationsToScylla] before dbNotifications iterations, len %v", len(dbNotifications))
+
+		for _, dbNotification := range dbNotifications {
+			eventTypes := database.GetNotificationTemplates(dbNotification.Type)
+
+			if len(eventTypes) == 0 {
+				logger.Info().Msgf("[MigrateNotificationsToScylla] eventTypes for \"%v\" not found", dbNotification.Type)
+				continue
+			}
+
+			eventType := ""
+
+			if len(eventTypes) > 1 {
+				switch dbNotification.Type {
+				case "push.paid_views.first":
+					if strings.Contains(dbNotification.Title, "Create your account") {
+						eventType = "first_guest_x_paid_views"
+					} else {
+						eventType = "first_x_paid_views"
+					}
+				case "push.guest.after_install":
+					if strings.Contains(dbNotification.Message, "Complete your account creation") {
+						eventType = "guest_after_install_first_push"
+					} else if strings.Contains(dbNotification.Message, "On Lit.it the more viral videos you watch the more your earn") {
+						eventType = "guest_after_install_second_push"
+					} else {
+						eventType = "guest_after_install_third_push"
+					}
+				case "push.user.after_signup":
+					if strings.Contains(dbNotification.Message, "Few things you need to know about Lit.it") {
+						eventType = "user_after_signup_first_push"
+					} else if strings.Contains(dbNotification.Message, "Check who earned the most for inviting friends to Lit.it") {
+						eventType = "user_after_signup_second_push"
+					} else if strings.Contains(dbNotification.Message, "Who earned the most LIT points? Check this out") {
+						eventType = "user_after_signup_third_push"
+					} else if strings.Contains(dbNotification.Message, "Check out TOP viral videos on Lit.it & earn LIT points") {
+						eventType = "user_after_signup_fourth_push"
+					} else {
+						eventType = "user_after_signup_fifth_push"
+					}
+				case "push.comment.vote":
+					if strings.Contains(dbNotification.Message, "disliked your comment") {
+						eventType = "comment_vote_dislike"
+					} else {
+						eventType = "comment_vote_like"
+					}
+				case "push.kyc.status":
+					if strings.Contains(dbNotification.Message, "Your identity verification has been approved") {
+						eventType = "kyc_status_verified"
+					} else {
+						eventType = "kyc_status_rejected"
+					}
 				}
 			} else {
-				groupedTemplateNotification, ok := groupedNotification[eventType]
+				eventType = eventTypes[0]
+			}
+
+			if len(eventType) == 0 {
+				logger.Info().Msgf("[MigrateNotificationsToScylla] eventTypes for \"%v\" not found is switch", dbNotification.Type)
+				continue
+			}
+
+			template, ok := templatesMap[eventType]
+			if !ok {
+				logger.Info().Msgf("[MigrateNotificationsToScylla] template for \"%v\" not found", eventType)
+				continue
+			}
+
+			logger.Info().Msgf("[MigrateNotificationsToScylla] eventType \"%v\" parsed for notification \"%v\"", eventType, dbNotification.Id.String())
+
+			customDataMarshalled, _ := json.Marshal(dbNotification.CustomData)
+			renderingVariablesMarshalled, _ := json.Marshal(dbNotification.RenderingVariables)
+			notificationMarshalled, _ := json.Marshal(dbNotification)
+
+			scyllaNotification := scylla.Notification{
+				UserId:             dbNotification.UserId,
+				EventType:          eventType,
+				CreatedAt:          dbNotification.CreatedAt,
+				EntityId:           dbNotification.UserId,
+				CustomData:         string(customDataMarshalled),
+				RenderingVariables: string(renderingVariablesMarshalled),
+				NotificationInfo:   string(notificationMarshalled),
+				Kind:               template.Kind,
+				NotificationsCount: 1,
+				Title:              dbNotification.Title,
+				Body:               dbNotification.Message,
+			}
+
+			switch eventType {
+			case "comment_reply":
+				scyllaNotification.Kind = "default"
+				scyllaNotification.RelatedEntityId = dbNotification.RelatedUserId.ValueOrZero()
+
+				if dbNotification.Comment != nil {
+					scyllaNotification.EntityId = dbNotification.Comment.ParentId.ValueOrZero()
+				}
+			case "comment_content_resource_create":
+				scyllaNotification.Kind = "default"
+				scyllaNotification.EntityId = dbNotification.ContentId.ValueOrZero()
+				scyllaNotification.RelatedEntityId = dbNotification.RelatedUserId.ValueOrZero()
+			case "comment_profile_resource_create":
+				scyllaNotification.Kind = "default"
+				scyllaNotification.EntityId = dbNotification.RelatedUserId.ValueOrZero()
+			case "spot_upload":
+				fallthrough
+			case "content_upload":
+				scyllaNotification.Kind = "default"
+				scyllaNotification.EntityId = dbNotification.ContentId.ValueOrZero()
+				scyllaNotification.RelatedEntityId = dbNotification.UserId
+			case "creator_status_rejected":
+				fallthrough
+			case "creator_status_approved":
+				fallthrough
+			case "creator_status_pending":
+				scyllaNotification.Kind = "content_creator"
+				scyllaNotification.EntityId = dbNotification.UserId
+			case "follow":
+				scyllaNotification.Kind = "user_follow"
+				scyllaNotification.EntityId = dbNotification.RelatedUserId.ValueOrZero()
+			case "kyc_status_verified":
+				fallthrough
+			case "kyc_status_rejected":
+				scyllaNotification.Kind = "default"
+				scyllaNotification.EntityId = dbNotification.UserId
+			case "content_like":
+				scyllaNotification.Kind = "default"
+				scyllaNotification.EntityId = dbNotification.ContentId.ValueOrZero()
+				scyllaNotification.RelatedEntityId = dbNotification.RelatedUserId.ValueOrZero()
+			case "push_admin":
+				scyllaNotification.Kind = "default"
+				scyllaNotification.EntityId = dbNotification.UserId
+			case "tip":
+				fallthrough
+			case "bonus_time":
+				fallthrough
+			case "bonus_followers":
+				scyllaNotification.Kind = "default"
+				scyllaNotification.EntityId = dbNotification.RelatedUserId.ValueOrZero()
+			case "comment_vote_dislike":
+				fallthrough
+			case "comment_vote_like":
+				scyllaNotification.Kind = "default"
+				scyllaNotification.EntityId = dbNotification.CommentId.ValueOrZero()
+				scyllaNotification.RelatedEntityId = dbNotification.RelatedUserId.ValueOrZero()
+			}
+
+			if template.IsGrouped {
+				groupedNotification, ok := groupedNotifications[dbNotification.UserId]
 				if !ok {
-					groupedNotifications[dbNotification.UserId][eventType] = map[int64]scylla.Notification{
-						scyllaNotification.EntityId: scyllaNotification,
+					groupedNotifications[dbNotification.UserId] = map[string]map[int64]scylla.Notification{
+						eventType: {
+							scyllaNotification.EntityId: scyllaNotification,
+						},
 					}
 				} else {
-					groupedTemplateNotificationEntity, ok := groupedTemplateNotification[scyllaNotification.EntityId]
+					groupedTemplateNotification, ok := groupedNotification[eventType]
 					if !ok {
-						groupedNotifications[dbNotification.UserId][eventType][scyllaNotification.EntityId] = scyllaNotification
+						groupedNotifications[dbNotification.UserId][eventType] = map[int64]scylla.Notification{
+							scyllaNotification.EntityId: scyllaNotification,
+						}
 					} else {
-						scyllaNotification.NotificationsCount = groupedTemplateNotificationEntity.NotificationsCount + 1
-						groupedNotifications[dbNotification.UserId][eventType][scyllaNotification.EntityId] = scyllaNotification
+						groupedTemplateNotificationEntity, ok := groupedTemplateNotification[scyllaNotification.EntityId]
+						if !ok {
+							groupedNotifications[dbNotification.UserId][eventType][scyllaNotification.EntityId] = scyllaNotification
+						} else {
+							scyllaNotification.NotificationsCount = groupedTemplateNotificationEntity.NotificationsCount + 1
+							groupedNotifications[dbNotification.UserId][eventType][scyllaNotification.EntityId] = scyllaNotification
+						}
 					}
 				}
 			}
-		}
 
-		ttl := timeNow.Unix() - scyllaNotification.CreatedAt.Unix()
+			ttl := timeNow.Unix() - scyllaNotification.CreatedAt.Unix()
 
-		if ttl <= 0 {
-			ttl = 7776000
-		}
+			if ttl <= 0 {
+				ttl = 7776000
+			}
 
-		if !template.IsGrouped {
-			scyllaNotificationsToUpdate = append(scyllaNotificationsToUpdate, scyllaNotification)
-		} else {
-			batch.Query(fmt.Sprintf("update notification_relation using ttl %v set event_applied = true "+
-				"where user_id = ? and event_type = ? and entity_id = ? and related_entity_id = ?", ttl),
-				scyllaNotification.UserId, scyllaNotification.EventType, scyllaNotification.EntityId,
-				scyllaNotification.RelatedEntityId)
+			if !template.IsGrouped {
+				scyllaNotificationsToUpdate = append(scyllaNotificationsToUpdate, scyllaNotification)
+			} else {
+				batch.Query(fmt.Sprintf("update notification_relation using ttl %v set event_applied = true "+
+					"where user_id = ? and event_type = ? and entity_id = ? and related_entity_id = ?", ttl),
+					scyllaNotification.UserId, scyllaNotification.EventType, scyllaNotification.EntityId,
+					scyllaNotification.RelatedEntityId)
+			}
 		}
 	}
 
