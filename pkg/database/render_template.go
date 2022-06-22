e07231e27c08d1acc14638f996609d64a520ba51
Dmitry Golodov <94372291+dimitriy5656@users.noreply.github.com>
2022-06-22 18:06:08 +0300

    BR-1953: implement settings api for admin (#204)

--- pkg/database/render_template.go
+++ pkg/database/render_template.go
@@ -3,14 +3,14 @@ package database
 import "time"
 
 type RenderTemplate struct {
-	Id        string
-	Kind      string
-	IsGrouped bool
-	Route     string
-	ImageUrl  string
+	Id        string `json:"id"`
+	Kind      string `json:"kind"`
+	IsGrouped bool   `json:"is_grouped"`
+	Route     string `json:"route"`
+	ImageUrl  string `json:"image_url"`
 
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 func (RenderTemplate) TableName() string {
