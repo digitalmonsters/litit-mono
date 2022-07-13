5a485cd245c8f5f685db6296f0a024afdf625487
dariiamoisol <94370133+dariiamoisol@users.noreply.github.com>
2022-07-13 14:21:00 +0300

    feat: BR-2126 content uploader wrapper (#322)
    * feat: BR-2126 content uploader wrapper
    
    * feat: BR-2126 change rpc to http
    
    * feat: close chan
    
    Co-authored-by: dariiamoisol <grimalskayad@gmail.com>

---
+++ wrappers/content_uploader/content_uploader.go
@@ -0,0 +1,60 @@
+package content_uploader
+
+import (
+	"fmt"
+	"github.com/digitalmonsters/go-common/boilerplate"
+	"github.com/digitalmonsters/go-common/common"
+	"github.com/digitalmonsters/go-common/rpc"
+	"github.com/digitalmonsters/go-common/wrappers"
+	"github.com/rs/zerolog/log"
+	"go.elastic.co/apm"
+	"time"
+)
+
+type IContentUploaderWrapper interface {
+	UploadContentInternal(url string, contentType string, data string, apmTransaction *apm.Transaction, forceLog bool) chan *rpc.RpcError
+}
+
+type ContentUploaderWrapper struct {
+	defaultTimeout time.Duration
+	apiUrl         string
+	serviceName    string
+	baseWrapper    *wrappers.BaseWrapper
+}
+
+func NewContentUploaderWrapper(config boilerplate.WrapperConfig) IContentUploaderWrapper {
+	timeout := 5 * time.Second
+
+	if config.TimeoutSec > 0 {
+		timeout = time.Duration(config.TimeoutSec) * time.Second
+	}
+
+	if len(config.ApiUrl) == 0 {
+		config.ApiUrl = "http://content-uploader"
+
+		log.Warn().Msgf("Api Url is missing for ContentUploader. Setting as default : %v", config.ApiUrl)
+	}
+
+	return &ContentUploaderWrapper{
+		defaultTimeout: timeout,
+		apiUrl:         fmt.Sprintf("%v/rpc-service", common.StripSlashFromUrl(config.ApiUrl)),
+		serviceName:    "content_uploader",
+		baseWrapper:    wrappers.GetBaseWrapper(),
+	}
+}
+
+func (w ContentUploaderWrapper) UploadContentInternal(url string, contentType string, data string, apmTransaction *apm.Transaction, forceLog bool) chan *rpc.RpcError {
+	resChan := make(chan *rpc.RpcError, 2)
+
+	go func() {
+		defer close(resChan)
+		rpcInternalResponse := <-w.baseWrapper.SendRequestWithRpcResponseFromAnyService(url,
+			"PUT",
+			contentType,
+			"upload content internal",
+			data, map[string]string{}, w.defaultTimeout, apmTransaction, w.serviceName, forceLog)
+		resChan <- rpcInternalResponse.Error
+	}()
+
+	return resChan
+}
