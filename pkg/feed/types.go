8224c952276d7e2cb4bc3914d3102ac5e9000182
a.porubai <a.porubai@gmail.com>
2022-08-03 12:05:37 +0300

    feat BR-2247: change feed resp

--- pkg/feed/types.go
+++ pkg/feed/types.go
@@ -1,6 +1,14 @@
 package feed
 
+import "github.com/digitalmonsters/music/pkg/frontend"
+
 type CursorPaging struct {
 	Before string `json:"before"`
 	After  string `json:"after"`
+}
+
+type ContentFeedResponse struct {
+	Data     []frontend.CreatorSongModel `json:"data"`
+	Paging   CursorPaging                `json:"paging"`
+	FeedType string                      `json:"feed_type"`
 }
