bbaeb82cb14d827790af6a85e3f0a556358562e4
ren_shark <irena.shaiekhova@gmail.com>
2022-08-04 18:16:01 +0300

    fix: BR-2219; user ids

--- pkg/converter/converter.go
+++ pkg/converter/converter.go
@@ -27,11 +27,15 @@ func NewConverter(userWrapper user_go.IUserGoWrapper, contentWrapper content.ICo
 
 func (c *Converter) MapFromDbAddCampaign(dbModels []database.AdCampaign, ctx context.Context) []common.AddModerationItem {
 	var userIds []int64
+	var usersMap = make(map[int64]bool)
+
 	var contentIds []int64
+
 	var items []*common.AddModerationItem
 
 	for _, dbModel := range dbModels {
 		contentIds = append(contentIds, dbModel.ContentId)
+		usersMap[dbModel.UserId] = false
 		items = append(items, &common.AddModerationItem{
 			Id:             dbModel.Id,
 			UserId:         dbModel.UserId,
@@ -55,9 +59,14 @@ func (c *Converter) MapFromDbAddCampaign(dbModels []database.AdCampaign, ctx con
 				time.Now().After(dbModel.CreatedAt.Add(time.Hour*time.Duration(configs.GetAppConfig().ADS_MODERATION_SLA))),
 		})
 	}
+
+	for userId := range usersMap {
+		userIds = append(userIds, userId)
+	}
+
 	routines := []chan error{
-		c.fillUsers(items, userIds, ctx),
 		c.fillContent(items, contentIds, ctx),
+		c.fillUsers(items, userIds, ctx),
 	}
 
 	for _, c := range routines {
