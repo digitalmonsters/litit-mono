09cb96fee1583dbce57c631385a8c0f584353c0a
logeshnatarajan <logeshnatarajan31@gmail.com>
2024-07-04 16:15:18 +0530

    added common modules for referral

--- wrappers/go_tokenomics/structs.go
+++ wrappers/go_tokenomics/structs.go
@@ -285,3 +285,236 @@ type WriteOffUserTokensForAdRequest struct {
 	AdCampaignId int64           `json:"ad_campaign_id"`
 	Amount       decimal.Decimal `json:"amount"`
 }
+
+type PointToVaultType string
+
+const (
+	EXTRA_BONUS                 = PointToVaultType("EXTRA_BONUS")
+	FRIENDS_INVITED             = PointToVaultType("FRIENDS_INVITED")
+	VIDEOS_SHARED               = PointToVaultType("VIDEOS_SHARED")
+	VIDEO_VIEWED                = PointToVaultType("VIDEO_VIEWED")
+	MY_VIDEO_VIEWS_GENERATED    = PointToVaultType("MY_VIDEO_VIEWS_GENERATED")
+	MY_SPOTS_VIEWS_GENERATED    = PointToVaultType("MY_SPOTS_VIEWS_GENERATED")
+	DAILY_TIME                  = PointToVaultType("DAILY_TIME")
+	DAILY_FOLLOWERS             = PointToVaultType("DAILY_FOLLOWERS")
+	WEEKLY_TIME                 = PointToVaultType("WEEKLY_TIME")
+	WEEKLY_FOLLOWERS            = PointToVaultType("WEEKLY_FOLLOWERS")
+	TAPJOY                      = PointToVaultType("TAPJOY")
+	APPLOVIN                    = PointToVaultType("APPLOVIN")
+	GOOGLE_ADS                  = PointToVaultType("GOOGLE_ADS")
+	IRONSOURCE                  = PointToVaultType("IRONSOURCE")
+	MEGA_BONUS                  = PointToVaultType("MEGA_BONUS")
+	FIRST_VIDEO                 = PointToVaultType("FIRST_VIDEO")
+	FIRST_SPOT                  = PointToVaultType("FIRST_SPOT")
+	FIRST_BIO_VIDEO             = PointToVaultType("FIRST_BIO_VIDEO")
+	DESCRIPTION_BONUS           = PointToVaultType("DESCRIPTION_BONUS")
+	AVATAR_ADDED                = PointToVaultType("AVATAR_ADDED")
+	SECOND_LEVEL_REFERRALS      = PointToVaultType("SECOND_LEVEL_REFERRALS")
+	EMAIL_MARKETING_ADDED       = PointToVaultType("EMAIL_MARKETING_ADDED")
+	DAILY_TOP_SPOT              = PointToVaultType("DAILY_TOP_SPOT")
+	WEEKLY_TOP_SPOT             = PointToVaultType("WEEKLY_TOP_SPOT")
+	SOCIAL_SUBS_TARGET_ACHIEVED = PointToVaultType("SOCIAL_SUBS_TARGET_ACHIEVED")
+	MONTHLY_TIME_MEGA_BONUS     = PointToVaultType("MONTHLY_TIME_MEGA_BONUS")
+	FIRST_SOCIAL_MEDIAS_ADDED   = PointToVaultType("FIRST_SOCIAL_MEDIAS_ADDED")
+	SURVEY                      = PointToVaultType("SURVEY")
+	FRIENDS_WATCHED_VIDEO       = PointToVaultType("FRIENDS_WATCHED_VIDEO")
+	WEEKLY_REFERRALS            = PointToVaultType("WEEKLY_REFERRALS")
+	FRIENDS_WATCHED_10HRS_VIDEO = PointToVaultType("FRIENDS_WATCHED_10HRS_VIDEO")
+	VERIFIED_REFERRALS          = PointToVaultType("VERIFIED_REFERRALS")
+	FRIENDS_WATCHED_10ADS       = PointToVaultType("FRIENDS_WATCHED_10ADS")
+)
+
+var ALL_FRONTEND_REWARD_TYPES = []PointToVaultType{
+	FRIENDS_INVITED,
+	FRIENDS_WATCHED_VIDEO,
+	SECOND_LEVEL_REFERRALS,
+	VIDEOS_SHARED,
+	VIDEO_VIEWED,
+	MY_VIDEO_VIEWS_GENERATED,
+	MY_SPOTS_VIEWS_GENERATED,
+	TAPJOY,
+	DAILY_TIME,
+	DAILY_FOLLOWERS,
+	WEEKLY_TIME,
+	WEEKLY_FOLLOWERS,
+	WEEKLY_REFERRALS,
+	DAILY_TOP_SPOT,
+	WEEKLY_TOP_SPOT,
+	MONTHLY_TIME_MEGA_BONUS,
+	SURVEY,
+	FRIENDS_WATCHED_10HRS_VIDEO,
+	VERIFIED_REFERRALS,
+	FRIENDS_WATCHED_10ADS,
+}
+
+type TokenomicsAppConfig struct {
+	TOKENOMICS_FEATURE_ONE_PAID_SHARE_PER_LINK_ENABLED bool
+	POINT_BONUS_VERIFY                                 decimal.Decimal // ok
+	POINT_BONUS_REFERRER_VERIFY                        decimal.Decimal // ok
+	POINT_BONUS_GRANDREFERRER_VERIFY                   decimal.Decimal
+
+	TOKEN_TOTAL_SUPPLY decimal.Decimal // important not to change
+	POINT_TO_TOKEN     decimal.Decimal // important not to change
+
+	TOKEN_RESERVE_SUPPLY                                   decimal.Decimal // important
+	POINT_VIEW_CC_USER                                     decimal.Decimal
+	POINT_VIEW_CC_GUEST                                    decimal.Decimal // to remove, not using
+	PAID_VIEW_MAX_PER_USER                                 int64
+	DAILY_MAX_AMOUNT_OF_PAID_VIEWS                         int64
+	PAID_VIEW_THRESHOLD_PERCENT                            decimal.Decimal
+	PAID_VIEW_THRESHOLD_TIME                               int64 // in seconds
+	PAID_VIEW_TIMEOUT_PER_USER                             int64 // todo check ; in days
+	POINT_VIEW_USER                                        decimal.Decimal
+	POINT_VIEW_SHARER                                      decimal.Decimal // todo check with igor
+	THRESHOLD_FOR_PERFORMANCE_BONUS_FOR_CONTENT_PAID_VIEWS int64           // todo will not work
+	TOKENS_FOR_PERFORMANCE_BONUS_OF_CONTENT_PAID_VIEWS     decimal.Decimal // todo will not work
+	PAID_SHARE_MAX_PER_USER                                int64
+	DAILY_MAX_AMOUNT_OF_PAID_SHARES                        int64
+	PAID_SHARE_TIMEOUT_PER_USER                            int64
+	PAID_SHARE_VIEW_THRESHOLD_PERCENT                      decimal.Decimal // todo check with igor;
+	PAID_SHARE_VIEW_THRESHOLD_TIME                         int64           // todo check with igor;
+	POINT_SHARE_USER                                       decimal.Decimal
+	POINTS_THRESHOLD_FOR_USER_WITHOUT_KYC                  decimal.Decimal
+	POINT_BASE_RATE                                        decimal.Decimal // important
+	POINT_SHARE_CC_USER                                    decimal.Decimal
+	POINT_SHARE_CC_GUEST                                   decimal.Decimal
+	TOKENOMICS_WITHDRAW_LIMIT_BEFORE_VESTING               decimal.Decimal
+
+	VISITORS_UNLOCKED_FEATURE_PRICE decimal.Decimal
+
+	POINT_BONUS_DAILY_FOLLOWERS_TARGET int64
+	POINT_BONUS_DAILY_FOLLOWERS        decimal.Decimal
+
+	POINT_BONUS_DAILY_TIME                      decimal.Decimal
+	POINT_BONUS_DAILY_TIME_TARGET               int64
+	MAX_TIP_POINTS_PER_DAY_FOR_USER_WITHOUT_KYC decimal.Decimal
+
+	POINT_BONUS_WEEKLY_TIME      decimal.Decimal
+	POINT_BONUS_WEEKLY_FOLLOWERS decimal.Decimal
+	WEEKLY_TIME_TARGET           int64
+	WEEKLY_FOLLOWERS_TARGET      int64
+
+	TOKENOMICS_FIRST_X_PAID_VIEWS_COUNT                  int64
+	TOKENOMICS_FIRST_X_PAID_VIEWS_AS_CONTENT_OWNER_COUNT int64
+	TOKENIMICS_REFERRAL_INCREASE_MULTIPLIER_STEP_1       decimal.Decimal
+	TOKENIMICS_REFERRAL_INCREASE_MULTIPLIER_STEP_2       decimal.Decimal
+	TOKENOMICS_REFERRAL_EARNED_POINTS_THRESHOLD_STEP_1   decimal.Decimal
+	TOKENOMICS_FIRST_X_PAID_VIEWS_COUNT_GENDER_PUSH      int64
+
+	GUEST_TOKENOMICS_FIRST_X_PAID_VIEWS_COUNT int64
+	GUEST_TOKENOMICS_FIRST_X_EARNED_POINTS    decimal.Decimal
+	GUEST_MAX_EARNED_POINTS_FOR_VIEWS         decimal.Decimal
+
+	MEGA_BONUS_TIME_LIMIT_AFTER_START_MINUTES   int64
+	MEGA_BONUS_REFERRALS_TARGET                 int64
+	MEGA_BONUS_REFERRALS_POINTS_AMOUNT          decimal.Decimal
+	MEGA_BONUS_REFERRALS_VIDEO_URL              string
+	MEGA_BONUS_REFERRALS_CONTENT_ID             string
+	MEGA_BONUS_MIN_TOTAL_WATCH_TIME_REQUIREMENT int64
+
+	POINT_BONUS_DESCRIPTION              decimal.Decimal
+	AVATAR_ADDED_BONUS_POINTS_AMOUNT     decimal.Decimal
+	POINT_BONUS_FIRST_VIDEO_UPLOADED     decimal.Decimal
+	POINT_BONUS_FIRST_BIO_VIDEO_UPLOADED decimal.Decimal
+
+	GUEST_AFTER_INSTALL_FIRST_PUSH_CONDITION_POINTS        decimal.Decimal
+	GUEST_AFTER_INSTALL_SECOND_PUSH_CONDITION_POINTS       decimal.Decimal
+	GUEST_AFTER_INSTALL_THIRD_PUSH_CONDITION_POINTS        decimal.Decimal
+	GUEST_AFTER_INSTALL_FIRST_PUSH_CONDITION_TIME_MINUTES  int64
+	GUEST_AFTER_INSTALL_SECOND_PUSH_CONDITION_TIME_MINUTES int64
+	GUEST_AFTER_INSTALL_THIRD_PUSH_CONDITION_TIME_MINUTES  int64
+
+	USER_AFTER_SIGNUP_FIRST_PUSH_CONDITION_CONTENT_ID    int64
+	USER_AFTER_SIGNUP_FIRST_PUSH_CONDITION_TIME_MINUTES  int64
+	USER_AFTER_SIGNUP_SECOND_PUSH_CONDITION_TIME_MINUTES int64
+	USER_AFTER_SIGNUP_THIRD_PUSH_CONDITION_TIME_MINUTES  int64
+	USER_AFTER_SIGNUP_FOURTH_PUSH_CONDITION_TIME_HOURS   int64
+	USER_AFTER_SIGNUP_FIFTH_PUSH_CONDITION_TIME_HOURS    int64
+	KYC_GRACE_PERIOD                                     int64 //days
+
+	AD_POINTS_AMOUNT_REWARD         decimal.Decimal // applovin
+	IRONSOURCE_REWARD               decimal.Decimal // ironsource
+	POINT_BONUS_FIRST_SPOT_UPLOADED decimal.Decimal
+
+	POINT_SPOTS_VIEW_USER                                        decimal.Decimal
+	POINT_SPOTS_VIEW_CC_USER                                     decimal.Decimal
+	DAILY_MAX_AMOUNT_OF_PAID_SPOTS_VIEWS                         int64
+	PAID_SPOTS_VIEW_THRESHOLD_PERCENT                            decimal.Decimal
+	PAID_SPOTS_VIEW_THRESHOLD_TIME                               int64
+	THRESHOLD_FOR_PERFORMANCE_BONUS_FOR_CONTENT_PAID_SPOTS_VIEWS decimal.Decimal
+	TOKENS_FOR_PERFORMANCE_BONUS_OF_CONTENT_PAID_SPOTS_VIEWS     decimal.Decimal
+
+	WITHDRAWAL_WHITELIST_USER_IDS        string
+	WITHDRAWAL_CREATION_INTERVAL_MINUTES int64
+	WITHDRAWAL_BLOCKED_COUNTRIES         string
+	WITHDRAWAL_FEATURE_ENABLED           bool
+
+	EMAIL_MARKETING_BONUS_POINTS_AMOUNT decimal.Decimal
+
+	DAILY_TOP_SPOT_BONUS  decimal.Decimal
+	WEEKLY_TOP_SPOT_BONUS decimal.Decimal
+
+	POINT_BONUS_SOCIAL_SUBS_TARGET_ACHIEVED decimal.Decimal
+
+	POINT_BONUS_MONTHLY_DAILY_TIME  int64
+	POINT_BONUS_MONTHLY_TIME_TARGET int64
+	POINT_BONUS_MONTHLY_TIME_REWARD decimal.Decimal
+	POINT_BONUS_MONTHLY_TIME_SKIP   int64
+	BONUS_MONTHLY_TIME_ENABLE       bool
+
+	TOKENOMICS_V2_PAID_VIEW_TIME_SECONDS        int
+	TOKENOMICS_V2_PAID_SESSION_TTL_SECONDS      int
+	BONUS_MONTHLY_TIME_DO_NOT_MISS_TIME_MINUTES int
+
+	FIRST_SOCIAL_MEDIA_QUANTITY              int
+	FIRST_N_SOCIAL_MEDIA_BONUS_POINTS_AMOUNT decimal.Decimal
+	CURRENT_TOKENOMICS_VERSION               int
+
+	WITHDRAWAL_USER_WEEKLY_LIMIT decimal.Decimal
+	SURVEY_SECRET_HASH           string
+	SURVEY_POINTS                decimal.Decimal
+
+	TOTAL_HOURS_REFERRED_WATCH_VIDEO        int
+	POINTS_BONUS_REFERRED_WATCH_VIDEO       decimal.Decimal
+	MAX_POINTS_BONUS_REFERRED_WATCH_VIDEO   decimal.Decimal
+	TOTAL_FRIENDS_REFERRED_IN_7_DAYS        int
+	POINTS_BONUS_FRIENDS_REFERRED_IN_7_DAYS decimal.Decimal
+
+	MONTHLY_CAPPED_POINT_WELCOME_TIER                      decimal.Decimal
+	MONTHLY_WITHDRAWAL_LIMIT_WELCOME_TIER                  int
+	MONTHLY_POINT_REQUIREMENT_WELCOME_TIER                 decimal.Decimal
+	MONTHLY_AD_WATCH_REQUIREMENT_WELCOME_TIER              int64
+	WEEKLY_TIME_SPENT_REQUIREMENT_WELCOME_TIER             int
+	WEEKLY_TIME_SPENT_PERCENT_POINT_DEDUCTION_WELCOME_TIER int
+
+	MONTHLY_PERCENT_POINT_BEGINNERS_TIER                     int
+	MONTHLY_WITHDRAWAL_LIMIT_BEGINNERS_TIER                  int
+	MONTHLY_POINT_REQUIREMENT_BEGINNERS_TIER                 decimal.Decimal
+	MONTHLY_AD_WATCH_REQUIREMENT_BEGINNERS_TIER              int64
+	WEEKLY_TIME_SPENT_REQUIREMENT_BEGINNERS_TIER             int
+	WEEKLY_TIME_SPENT_PERCENT_POINT_DEDUCTION_BEGINNERS_TIER int
+
+	MONTHLY_PERCENT_POINT_INTERMEDIARY_TIER                     int
+	MONTHLY_WITHDRAWAL_LIMIT_INTERMEDIARY_TIER                  int
+	MONTHLY_POINT_REQUIREMENT_INTERMEDIARY_TIER                 decimal.Decimal
+	MONTHLY_AD_WATCH_REQUIREMENT_INTERMEDIARY_TIER              int64
+	WEEKLY_TIME_SPENT_REQUIREMENT_INTERMEDIARY_TIER             int
+	WEEKLY_TIME_SPENT_PERCENT_POINT_DEDUCTION_INTERMEDIARY_TIER int
+
+	MONTHLY_PERCENT_POINT_ADVANCED_TIER                     int
+	MONTHLY_WITHDRAWAL_LIMIT_ADVANCED_TIER                  int
+	MONTHLY_POINT_REQUIREMENT_ADVANCED_TIER                 decimal.Decimal
+	MONTHLY_AD_WATCH_REQUIREMENT_ADVANCED_TIER              int64
+	WEEKLY_TIME_SPENT_REQUIREMENT_ADVANCED_TIER             int
+	WEEKLY_TIME_SPENT_PERCENT_POINT_DEDUCTION_ADVANCED_TIER int
+
+	MONTHLY_PERCENT_POINT_SUPERSTAR_TIER                     int
+	MONTHLY_WITHDRAWAL_LIMIT_SUPERSTAR_TIER                  int
+	MONTHLY_POINT_REQUIREMENT_SUPERSTAR_TIER                 decimal.Decimal
+	MONTHLY_AD_WATCH_REQUIREMENT_SUPERSTAR_TIER              int64
+	WEEKLY_TIME_SPENT_REQUIREMENT_SUPERSTAR_TIER             int
+	WEEKLY_TIME_SPENT_PERCENT_POINT_DEDUCTION_SUPERSTAR_TIER int
+
+	COUNTRY_RATE_CONVERSION_ENABLED bool
+	WITHDRAWAL_RATE                 decimal.Decimal
+}
