5e15ebae2d66bd2e9ad49ebfa1dc5616f42a986b
Aarav Arora <aaravarora.works@gmail.com>
2024-08-05 20:55:55 +0530

    fix: thumbnail

--- utils/content.go
+++ utils/content.go
@@ -2,19 +2,20 @@ package utils
 
 import (
 	"fmt"
+
 	"github.com/digitalmonsters/ads-manager/configs"
 )
 
 func GetAnimUrl(videoId string) string {
-	return fmt.Sprintf("%v/%v/%v/a.webp", configs.CDN_BASE, configs.PREFIX_CONTENT, videoId)
+	return fmt.Sprintf("%v/%v/preview.webp", configs.CDN_BASE, videoId)
 }
 
 func GetThumbnailUrl(videoId string) string {
-	return fmt.Sprintf("%v/%v/%v/t.jpg", configs.CDN_BASE, configs.PREFIX_CONTENT, videoId)
+	return fmt.Sprintf("%v/%v/thumbnail.jpg", configs.CDN_BASE, videoId)
 }
 
 func GetVideoUrl(videoId string) string {
-	return fmt.Sprintf("%v/%v/%v/v.mp4", configs.CDN_BASE, configs.PREFIX_CONTENT, videoId)
+	return fmt.Sprintf("%v/%v/playlist.m3u8", configs.CDN_BASE, videoId)
 }
 
 func GetIsVertical(width, height int) bool {
