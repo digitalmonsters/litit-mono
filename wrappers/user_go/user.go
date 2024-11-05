7910c239574e3b3795ba86e4d450fa2d97e29441
Aarav Arora <aaravarora.works@gmail.com>
2024-11-05 17:38:34 +0530

    chore: user-go wrapper

--- wrappers/user_go/user.go
+++ wrappers/user_go/user.go
@@ -47,7 +47,7 @@ type IUserGoWrapper interface {
 	UpdatePetAlbum(petId int64, videoId string, userId int64, ctx context.Context, forceLog bool) chan wrappers.GenericResponseChan[any]
 	GetFriendListData(userId int64, ctx context.Context, forceLog bool) chan wrappers.GenericResponseChan[GetFriendListDataResponse]
 	GetUsersWithFollowers(userIds []int64, ctx context.Context, forceLog bool) chan wrappers.GenericResponseChan[GetFriendListDataResponse]
-	GetIsRequested(userId int64, friendId int64, ctx context.Context, forceLog bool) chan wrappers.GenericResponseChan[IsRequestedResponse]
+	GetIsRequested(userId int64, ctx context.Context, forceLog bool) chan wrappers.GenericResponseChan[GetFriendListDataResponse]
 }
 
 //goland:noinspection GoNameStartsWithPackageName
@@ -495,10 +495,9 @@ func (w UserGoWrapper) GetUsersWithFollowers(userIds []int64, ctx context.Contex
 
 }
 
-func (w UserGoWrapper) GetIsRequested(userId int64, friendId int64, ctx context.Context, forceLog bool) chan wrappers.GenericResponseChan[IsRequestedResponse] {
-	return wrappers.ExecuteRpcRequestAsync[IsRequestedResponse](w.baseWrapper, w.serviceApiUrl,
+func (w UserGoWrapper) GetIsRequested(userId int64, ctx context.Context, forceLog bool) chan wrappers.GenericResponseChan[GetFriendListDataResponse] {
+	return wrappers.ExecuteRpcRequestAsync[GetFriendListDataResponse](w.baseWrapper, w.serviceApiUrl,
 		"GetIsRequested", IsRequestedRequest{
-			UserId:   userId,
-			FriendId: friendId,
+			UserId: userId,
 		}, map[string]string{}, w.defaultTimeout, apm.TransactionFromContext(ctx), w.serviceName, forceLog)
 }
