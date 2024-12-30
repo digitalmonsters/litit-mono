690026b8c38d037d06bf7c05338dafd3353afa12
logeshnatarajan <logeshnatarajan31@gmail.com>
2024-12-30 14:19:07 +0530

    added sound abject on firebase

--- pkg/firebase/firebase.go
+++ pkg/firebase/firebase.go
@@ -46,6 +46,7 @@ func Initialize(ctx context.Context, serviceAccountJSON string) *FirebaseClient 
 
 // SendNotification sends a push notification to a specific device token
 func (f *FirebaseClient) SendNotification(ctx context.Context, deviceToken, title, body string, data map[string]string) (string, error) {
+	data["sound"] = "Sweet.mp3"
 	message := &messaging.Message{
 		Token: deviceToken,
 		Notification: &messaging.Notification{
