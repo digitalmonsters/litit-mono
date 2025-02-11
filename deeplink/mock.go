5dd1411c2af747b5b6b62206aa256ff4c2c5a874
Abhijith Ganesh <work@abhijithganesh.com>
2025-02-11 18:06:26 +0530

    Addition of Firebase to parent service
    Signed-off-by: Abhijith Ganesh <work@abhijithganesh.com>

--- deeplink/mock.go
+++ deeplink/mock.go
@@ -3,11 +3,12 @@ package deeplink
 import "github.com/digitalmonsters/go-common/eventsourcing"
 
 type ServiceMock struct {
-	GetVideoShareLinkFn              func(contentId int64, contentType eventsourcing.ContentType, userId int64, referralCode string) (string, error)
-	GetPreviewShareLinkFn            func(contentId int64, contentType eventsourcing.ContentType, uri string, userId int64, referralCode string) (string, error)
-	GetPetVideoShareLinkFn           func(contentId int64, contentType eventsourcing.ContentType, userId int64, referralCode string, petType int64, petId int64, petName string) (string, error)
-	GetVideoShareLinkWithMetaFn      func(contentId int64, contentType eventsourcing.ContentType, userId int64, referralCode string, title string, description string, previewThumbnail string) (string, error)
-	GenerateBranchDeeplinkWithMetaFn func(link string, title string, description string, previewThumbnail string) (string, error)
+	GetVideoShareLinkFn                func(contentId int64, contentType eventsourcing.ContentType, userId int64, referralCode string) (string, error)
+	GetPreviewShareLinkFn              func(contentId int64, contentType eventsourcing.ContentType, uri string, userId int64, referralCode string) (string, error)
+	GetPetVideoShareLinkFn             func(contentId int64, contentType eventsourcing.ContentType, userId int64, referralCode string, petType int64, petId int64, petName string) (string, error)
+	GetVideoShareLinkWithMetaFn        func(contentId int64, contentType eventsourcing.ContentType, userId int64, referralCode string, title string, description string, previewThumbnail string) (string, error)
+	GenerateBranchDeeplinkWithMetaFn   func(link string, title string, description string, previewThumbnail string) (string, error)
+	GenerateFirebaseDeeplinkWithMetaFn func(link string, title string, description string, previewThumbnail string) (string, error)
 }
 
 func (m ServiceMock) GenerateBranchDeeplinkWithMeta(link string, title string, description string, previewThumbnail string) (string, error) {
@@ -28,6 +29,10 @@ func (m ServiceMock) GetPetVideoShareLink(contentId int64, contentType eventsour
 
 func (m ServiceMock) GetVideoShareLinkWithMeta(contentId int64, contentType eventsourcing.ContentType, userId int64, referralCode string, title string, description string, previewThumbnail string) (string, error) {
 	return m.GetVideoShareLinkWithMetaFn(contentId, contentType, userId, referralCode, title, description, previewThumbnail)
+}
+
+func (m ServiceMock) GenerateFirebaseDeeplinkWithMeta(link string, title string, description string, previewThumbnail string) (string, error) {
+	return m.GenerateFirebaseDeeplinkWithMetaFn(link, title, description, previewThumbnail)
 }
 
 func GetMock() IService {
