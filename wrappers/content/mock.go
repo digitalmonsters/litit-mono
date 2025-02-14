fadf2fb68f77c4d19a13594c45c1991026196a23
Aarav Arora <aaravarora.works@gmail.com>
2025-02-14 14:26:10 +0530

    intro single

--- wrappers/content/mock.go
+++ wrappers/content/mock.go
@@ -88,9 +88,9 @@ func (w *ContentWrapperMock) InsertMusicContent(content MusicContentRequest, ctx
 	return w.InsertMusicContentFn(content, ctx, forceLog)
 }
 
-// func (w *ContentWrapperMock) GetIfIntroExists(ctx context.Context, userId int64) chan wrappers.GenericResponseChan[[]IntroExists] {
-// 	panic("implement me")
-// }
+func (w *ContentWrapperMock) GetIfIntroExists(ctx context.Context, userId int64) chan wrappers.GenericResponseChan[[]IntroExists] {
+	panic("implement me")
+}
 
 func (w *ContentWrapperMock) GetAllUploadCount(ctx context.Context, userId int64) chan wrappers.GenericResponseChan[UploadCountResponse] {
 	panic("implement me")
